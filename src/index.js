25c40ce826e8ebff764c0f48541f87fb5c3b1024
Ilyas Gibadullin <ilyas.gibadullin.199819987@gmail.com>
2023-03-06 20:31:39 +0300

    feat: 🎸 Connect DB server

--- src/index.js
+++ src/index.js
@@ -1,11 +1,40 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:admin@cluster0.hngcx2r.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("DB OK");
+  })
+  .catch((err) => console.log("DB error", err.message));
 
 const PORT = 5555;
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+app.post("/auth/login", (req, res) => {
+  console.log(req.body);
+
+  const token = jwt.sign(
+    {
+      email: req.body.email,
+      fullName: "ilyas Gosling",
+    },
+    "secret123"
+  );
+
+  res.json({
+    success: true,
+    token,
+  });
 });
 
 app.listen(PORT, (err) => {
